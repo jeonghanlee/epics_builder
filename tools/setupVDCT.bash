86c5aa5675bcaebef522efca3c4c0a9564376103
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-08-13 23:53:13 +0200

    add VDCT setup env

--- tools/setupVDCT.bash
+++ tools/setupVDCT.bash
@@ -10,42 +10,69 @@
 
 wget_options="wget -c"
 
-temp_downloads=""
-	   
+
+function pushd { builtin pushd "$@" > /dev/null; }
+function popd  { builtin popd  "$@" > /dev/null; }
+
+function die
+{
+    error=${1:-1}
+    ## exits with 1 if error number not given
+    shift
+    [ -n "$*" ] &&
+	printf "%s%s: %s\n" "$scriptname" ${version:+" ($version)"} "$*" >&2
+    exit "$error"
+}
+
+
+
 make_vdct()
 {
     local vdct_version="2.7.0"
-    local vdct_filename=VisualDCT-${vdct_version}-distribution.zip
-    local vdct_full_path="${EPICS_EXTENSION}/VisualDCT"
-    local run_script=""
-    local vdct_jar="VisualDCT.jar"
-    local vdct_bin=""
+    local vdct_name="VisualDCT-${vdct_version}"
+    local vdct_filename="${vdct_name}-distribution.zip"
+    local vdct_jar="${vdct_name}.jar"
+    local vdct_target_jar="VisualDCT.jar"
+    local vdct_full_path="${EPICS_EXTENSIONS}/src"
 
-    $wget_options https://github.com/epics-extensions/VisualDCT/releases/download/${vdct_filename}
+    local vdct_bin=${EPICS_EXTENSIONS}/bin/${EPICS_HOST_ARCH}/vdct
+    local vdct_home=${vdct_full_path}/${vdct_name}
+    local vdct_script=${vdct_home}/vdct.bash
+  
+    local temp_downloads=${SRC_PATH}/.tmp
+
+    mkdir -p ${temp_downloads}
+    
+    pushd ${temp_downloads}
+    
+    $wget_options https://github.com/epics-extensions/VisualDCT/releases/download/v${vdct_version}/${vdct_filename} ||  die 1 "${FUNCNAME[*]} : Failed at wget: Please check it" ;
 
     # Force (-o option) to overwrite when the target files are in the target directory,
     # so it will prevent the duplicated entries in the below sed procedure.
     #
     unzip -oq -d ${vdct_full_path} ${vdct_filename}
 
-    vdct_home=${vdct_full_path}/${vdct_version}
-    run_script=${vdct_home}/runScript
-    vdct_bin=${EPICS_EXTENSIONS}/bin/${EPICS_HOST_ARCH}/vdct
+    popd
 
-    #
-    # fix the path for VisualDCT.jar location in runScript
-    # in order to run it globally.
-    #
-    sed -i "s|${vdct_jar_name}|${vdct_home}/${vdct_jar_name}|g" ${run_script}
+    # unzip directory has ${vdct_name}
+
+    cat > ${vdct_script} <<EOF
+#!/bin/sh -e
+
+java -cp ${vdct_home}/${vdct_jar} -DEPICS_DB_INCLUDE_PATH=\$EPICS_DB_INCLUDE_PATH com.cosylab.vdct.VisualDCT \$*
+
+EOF
+    
     #
     # add the execute permission for runScript
     #
-    chmod +x ${run_script}
+    chmod +x ${vdct_script}
     #
-    # force to make a symbolic link for vdct in vdc_target/bin
+    # force to make a symbolic link for vdct in vdct_bin
     #
-    ln -sf ${run_script} ${vdct_bin}
-}
+    ln -sf ${vdct_script} ${vdct_bin}
+ }
+
 
 
 THIS_SRC=${BASH_SOURCE[0]}
@@ -62,20 +89,15 @@ if [[ $SRC_PATH == *${REAL_SRC_PATH}* ]]; then
     sleep 5
     cd  ${SRC_PATH}/../../
     source setEpicsEnv.bash
-
 else
     source ${SRC_PATH}/setEpicsEnv.bash
 fi
 
-temp_downloads=${SRC_PATH}/.tmp
 
-mkdir -p ${temp_downloads}
-
-pushd ${temp_downloads}
 make_vdct
-popd
 
-rm -rf ${temp_downloads}
+# do we need to clean? 
+#rm -rf ${temp_downloads}
 
 
 
